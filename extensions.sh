e0ac928a492fe094fac4efcd2d1c555a7201c420
Rasmus Bundsgaard <xxr@live.dk>
2022-01-24 10:54:39 +0100

    Create extensions.sh

---
+++ extensions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+code --install-extension abusaidm.html-snippets
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension bungcip.better-toml
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension DotJoshJohnson.xml
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GitLab.gitlab-workflow
+code --install-extension ikappas.phpcs
+code --install-extension johnpapa.vscode-peacock
+code --install-extension johnsoncodehk.volar
+code --install-extension lokalise.i18n-ally
+code --install-extension matklad.rust-analyzer
+code --install-extension MehediDracula.php-namespace-resolver
+code --install-extension mrmlnc.vscode-postcss-sorting
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension octref.vetur
+code --install-extension onecentlin.laravel-blade
+code --install-extension phproberto.vscode-php-getters-setters
+code --install-extension redhat.vscode-commons
+code --install-extension redhat.vscode-xml
+code --install-extension richie5um2.vscode-sort-json
+code --install-extension rvest.vs-code-prettier-eslint
+code --install-extension silvenon.mdx
+code --install-extension SimonSiefke.prettier-vscode
+code --install-extension SimonSiefke.svg-preview
+code --install-extension stylelint.vscode-stylelint
+code --install-extension tarik02.php-imports
+code --install-extension whatwedo.twig
+code --install-extension wholroyd.jinja
